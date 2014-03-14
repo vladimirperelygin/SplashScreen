c5b6e80f2fb329ae4c355eaf1e39b81705ac2cc9
Vladimir Perelygin <vladimir.perelygin@aiesec.net>
2014-03-15 00:30:51 +0400

    Сделано: listView, адекватно реагирует при повороте, при нажатии назад выходит.

--- firstApp/src/com/example/firstapp/Home_Screen.java
+++ firstApp/src/com/example/firstapp/Home_Screen.java
@@ -5,7 +5,9 @@ import java.util.Arrays;
 
 import android.app.Activity;
 import android.app.ListActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
@@ -15,7 +17,7 @@ public class Home_Screen extends Activity {
 	private String _data[] =
 	{ "1", "2", "3", "4", "5", "6", "7", "8", "9", "10",
      "11","12","13", "14", "15", "16", "17", "18", "19", "20" };
-	
+
 	ArrayAdapter<String> adapter;
 	  
 	@Override
@@ -24,10 +26,18 @@ public class Home_Screen extends Activity {
 	    super.onCreate(savedInstanceState);
 	    setContentView(R.layout.home_screen);
 	    _list1=(ListView)findViewById(R.id.listView1);
-		adapter =  new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1 , _data);
+	    adapter =  new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1 , _data);
 	    _list1.setAdapter(adapter);
-	    
-	    
+	  
+	   
 	  }
+	@Override
+	public void onBackPressed() {
+		Intent intent = new Intent(Intent.ACTION_MAIN);
+        intent.addCategory(Intent.CATEGORY_HOME);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+	}
+	
 }
 
--- firstApp/src/com/example/firstapp/Splash_Screen.java
+++ firstApp/src/com/example/firstapp/Splash_Screen.java
@@ -4,8 +4,10 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 import android.os.Bundle;
+import android.os.Handler;
 
 import android.app.Activity;
+import android.content.Intent;
 
 import android.view.Menu;
 
@@ -16,39 +18,43 @@ public class Splash_Screen extends Activity  {
 		protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		
-        setContentView(R.layout.splash_screen);
-        splash();
-      
+        setContentView(R.layout.splash_screen);     
+        ShownHomeScreen();
+
         } 
 	
-       public void splash()
-       {
-    	   Timer t = new Timer();  
+       
+	public void ShownHomeScreen()
+	{
+		 final Handler handler = new Handler();
+	        Runnable incrementRunnable = new Runnable() {
 
-           TimerTask task = new TimerTask() {  
-
-            @Override  
-            public void run() {  
-             runOnUiThread(new Runnable() {  
-
-              @Override  
-              public void run() {  
-           	  setContentView(R.layout.home_screen); 
-              }  
-            });  
-            }  
-          }; 
-            t.schedule( task, 2000 ); 
-       }
-
+	            @Override
+	            public void run() {
+	            	 Intent InHome = new Intent(Splash_Screen.this,Home_Screen.class);
+	                 Splash_Screen.this.startActivity(InHome);
+	                handler.postDelayed(this, 20000);
+	            }
+	        };
+	      
+			handler.postDelayed(incrementRunnable, 2000);
+	}
 	
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
+	public void onBackPressed() {
+		Intent intent = new Intent(Intent.ACTION_MAIN);
+        intent.addCategory(Intent.CATEGORY_HOME);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
 	}
 
+   	@Override
+   	public boolean onCreateOptionsMenu(Menu menu) {
+   		// Inflate the menu; this adds items to the action bar if it is present.
+   		getMenuInflater().inflate(R.menu.main, menu);
+   		return true;
+   	}
+	
 	
 
 }
