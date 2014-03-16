de4801124f6da981fff898dd76752739ad5895b3
Vladimir Perelygin <vladimir.perelygin@aiesec.net>
2014-03-16 16:11:52 +0400

    Исправил замечания (автоформат кода, кнопка back, layout-land, переменные)

--- SplashScreen/src/com/example/firstapp/HomeScreenActivity.java
+++ SplashScreen/src/com/example/firstapp/HomeScreenActivity.java
@@ -1,43 +1,30 @@
 package com.example.firstapp;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-
 import android.app.Activity;
-import android.app.ListActivity;
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.Toast;
 
 public class HomeScreenActivity extends Activity {
-	private ListView _list1;
-	private String _data[] =
-	{ "1", "2", "3", "4", "5", "6", "7", "8", "9", "10",
-     "11","12","13", "14", "15", "16", "17", "18", "19", "20" };
 
-	ArrayAdapter<String> adapter;
-	  
+	private String _data[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9",
+			"10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20" };
+
 	@Override
-	  protected void onCreate(Bundle savedInstanceState) {
-		 
-	    super.onCreate(savedInstanceState);
-	    setContentView(R.layout.home_screen);
-	    _list1=(ListView)findViewById(R.id.listView1);
-	    adapter =  new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1 , _data);
-	    _list1.setAdapter(adapter);
-	  
-	   
-	  }
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.home_screen);
+		ListView _list1 = (ListView) findViewById(R.id.listView1);
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+				android.R.layout.simple_list_item_1, _data);
+		_list1.setAdapter(adapter);
+
+	}
+
 	@Override
 	public void onBackPressed() {
-		Intent intent = new Intent(Intent.ACTION_MAIN);
-        intent.addCategory(Intent.CATEGORY_HOME);
-        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        startActivity(intent);
-	}
-	
-}
 
+		this.moveTaskToBack(true);
+	}
+
+}
--- SplashScreen/src/com/example/firstapp/SplashScreenActivity.java
+++ SplashScreen/src/com/example/firstapp/SplashScreenActivity.java
@@ -1,60 +1,51 @@
 package com.example.firstapp;
 
-import java.util.Timer;
-import java.util.TimerTask;
-
-import android.os.Bundle;
-import android.os.Handler;
-
 import android.app.Activity;
 import android.content.Intent;
-
+import android.os.Bundle;
+import android.os.Handler;
 import android.view.Menu;
 
+public class SplashScreenActivity extends Activity {
 
-public class SplashScreenActivity extends Activity  {
-	
 	@Override
-		protected void onCreate(Bundle savedInstanceState) {
+	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
-        setContentView(R.layout.splash_screen);     
-        startShowHomeScreen();
 
-        } 
-	
-       
-	public void startShowHomeScreen()
-	{
-		 final Handler handler = new Handler();
-	        Runnable incrementRunnable = new Runnable() {
+		setContentView(R.layout.splash_screen);
+		startShowHomeScreen();
 
-	            @Override
-	            public void run() {
-	            	 Intent InHome = new Intent(SplashScreenActivity.this,HomeScreenActivity.class);
-	                 SplashScreenActivity.this.startActivity(InHome);
-	                handler.postDelayed(this, 20000);
-	            }
-	        };
-	      
-			handler.postDelayed(incrementRunnable, 2000);
 	}
-	
+
+	public void startShowHomeScreen() {
+		final Handler handler = new Handler();
+		Runnable incrementRunnable = new Runnable() {
+
+			@Override
+			public void run() {
+				Intent InHome = new Intent(SplashScreenActivity.this,
+						HomeScreenActivity.class);
+				SplashScreenActivity.this.startActivity(InHome);
+				handler.postDelayed(this, 20000);
+			}
+		};
+
+		handler.postDelayed(incrementRunnable, 2000);
+	}
+
 	@Override
 	public void onBackPressed() {
 		Intent intent = new Intent(Intent.ACTION_MAIN);
-        intent.addCategory(Intent.CATEGORY_HOME);
-        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        startActivity(intent);
+		intent.addCategory(Intent.CATEGORY_HOME);
+		intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+		startActivity(intent);
 	}
 
-   	@Override
-   	public boolean onCreateOptionsMenu(Menu menu) {
-   		// Inflate the menu; this adds items to the action bar if it is present.
-   		getMenuInflater().inflate(R.menu.main, menu);
-   		return true;
-   	}
-	
-	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
 
 }
